b76e54938ef9f56dbb04e267a15a18784ecd52b7
Jamie Benjamin <bennythedev@gmail.com>
2022-06-09 08:32:28 -0500

    commit tree 7898bec661bfbd9ee8c35acd3501642d3080b432

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.append();
